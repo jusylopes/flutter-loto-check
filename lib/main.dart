7b73fd690ad53d699efc3ef83f27b070bbd913f5
jusylopes <jusylopes@outlook.com>
2023-11-07 11:12:12 -0300

    add: first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_loto_check/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: const [],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Loto Check',
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
