61455b747209f9f7702dafb641ed13d451890622
jusylopes <jusylopes@outlook.com>
2023-11-23 21:33:09 -0300

    refactor: bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_loto_check/repository/loto_reporitory.dart';
 import 'package:flutter_loto_check/screen/home_screen.dart';
+
+import 'bloc/register/register_blocs_exports.dart';
+import 'bloc/search/search_loto_blocs_exports.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +18,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: const [],
+      providers: [
+        BlocProvider<RegisterLotoBloc>(
+          create: (_) => RegisterLotoBloc(
+            repository: LotteryRepository(
+              dio: Dio(),
+            ),
+          ),
+        ),
+        BlocProvider<SearchLotoBloc>(
+          create: (_) => SearchLotoBloc(
+            repository: LotteryRepository(
+              dio: Dio(),
+            ),
+          ),
+        ),
+      ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Loto Check',
