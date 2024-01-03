91a04bb72491b21da40d13a16fe42b18bcb573ef
jusylopes <jusylopes@outlook.com>
2024-01-03 08:45:59 -0300

    refactor: model

--- lib/model/mega_sena_model.dart
+++ lib/model/mega_sena_model.dart
@@ -1,285 +1,189 @@
 import 'dart:convert';
 
-MegaSenaModel modelMegaSenaFromJson(String str) =>
-    MegaSenaModel.fromJson(json.decode(str));
+ModelMegaSena modelMegaSenaFromJson(String str) => ModelMegaSena.fromJson(json.decode(str));
 
-String modelMegaSenaToJson(MegaSenaModel data) => json.encode(data.toJson());
+String modelMegaSenaToJson(ModelMegaSena data) => json.encode(data.toJson());
 
-class MegaSenaModel {
-  final bool acumulado;
-  final String dataApuracao;
-  final String dataProximoConcurso;
-  final List<String> dezenasSorteadasOrdemSorteio;
-  final bool exibirDetalhamentoPorCidade;
-  final dynamic id;
-  final int indicadorConcursoEspecial;
-  final List<String> listaDezenas;
-  final dynamic listaDezenasSegundoSorteio;
-  final List<dynamic> listaMunicipioUfGanhadores;
-  final List<ListaRateioPremio> listaRateioPremio;
-  final dynamic listaResultadoEquipeEsportiva;
-  final String localSorteio;
-  final String nomeMunicipioUfSorteio;
-  final String nomeTimeCoracaoMesSorte;
-  final int numero;
-  final int numeroConcursoAnterior;
-  final int numeroConcursoFinal05;
-  final int numeroConcursoProximo;
-  final int numeroJogo;
-  final String observacao;
-  final dynamic premiacaoContingencia;
-  final String tipoJogo;
-  final int tipoPublicacao;
-  final bool ultimoConcurso;
-  final double valorArrecadado;
-  final double valorAcumuladoConcurso05;
-  final double valorAcumuladoConcursoEspecial;
-  final double valorAcumuladoProximoConcurso;
-  final double valorEstimadoProximoConcurso;
-  final double valorSaldoReservaGarantidora;
-  final double valorTotalPremioFaixaUm;
+class ModelMegaSena {
+    final String loteria;
+    final int concurso;
+    final String data;
+    final String local;
+    final List<String> dezenasOrdemSorteio;
+    final List<String> dezenas;
+    final List<dynamic> trevos;
+    final String timeCoracao;
+    final String mesSorte;
+    final List<Premiacoe> premiacoes;
+    final List<dynamic> estadosPremiados;
+    final String observacao;
+    final bool acumulou;
+    final int proximoConcurso;
+    final String dataProximoConcurso;
+    final List<dynamic> localGanhadores;
+    final double valorArrecadado;
+    final double valorAcumuladoConcurso05;
+    final double valorAcumuladoConcursoEspecial;
+    final double valorAcumuladoProximoConcurso;
+    final double valorEstimadoProximoConcurso;
 
-  MegaSenaModel({
-    required this.acumulado,
-    required this.dataApuracao,
-    required this.dataProximoConcurso,
-    required this.dezenasSorteadasOrdemSorteio,
-    required this.exibirDetalhamentoPorCidade,
-    required this.id,
-    required this.indicadorConcursoEspecial,
-    required this.listaDezenas,
-    required this.listaDezenasSegundoSorteio,
-    required this.listaMunicipioUfGanhadores,
-    required this.listaRateioPremio,
-    required this.listaResultadoEquipeEsportiva,
-    required this.localSorteio,
-    required this.nomeMunicipioUfSorteio,
-    required this.nomeTimeCoracaoMesSorte,
-    required this.numero,
-    required this.numeroConcursoAnterior,
-    required this.numeroConcursoFinal05,
-    required this.numeroConcursoProximo,
-    required this.numeroJogo,
-    required this.observacao,
-    required this.premiacaoContingencia,
-    required this.tipoJogo,
-    required this.tipoPublicacao,
-    required this.ultimoConcurso,
-    required this.valorArrecadado,
-    required this.valorAcumuladoConcurso05,
-    required this.valorAcumuladoConcursoEspecial,
-    required this.valorAcumuladoProximoConcurso,
-    required this.valorEstimadoProximoConcurso,
-    required this.valorSaldoReservaGarantidora,
-    required this.valorTotalPremioFaixaUm,
-  });
+    ModelMegaSena({
+        required this.loteria,
+        required this.concurso,
+        required this.data,
+        required this.local,
+        required this.dezenasOrdemSorteio,
+        required this.dezenas,
+        required this.trevos,
+        required this.timeCoracao,
+        required this.mesSorte,
+        required this.premiacoes,
+        required this.estadosPremiados,
+        required this.observacao,
+        required this.acumulou,
+        required this.proximoConcurso,
+        required this.dataProximoConcurso,
+        required this.localGanhadores,
+        required this.valorArrecadado,
+        required this.valorAcumuladoConcurso05,
+        required this.valorAcumuladoConcursoEspecial,
+        required this.valorAcumuladoProximoConcurso,
+        required this.valorEstimadoProximoConcurso,
+    });
 
-  MegaSenaModel copyWith({
-    bool? acumulado,
-    String? dataApuracao,
-    String? dataProximoConcurso,
-    List<String>? dezenasSorteadasOrdemSorteio,
-    bool? exibirDetalhamentoPorCidade,
-    dynamic id,
-    int? indicadorConcursoEspecial,
-    List<String>? listaDezenas,
-    dynamic listaDezenasSegundoSorteio,
-    List<dynamic>? listaMunicipioUfGanhadores,
-    List<ListaRateioPremio>? listaRateioPremio,
-    dynamic listaResultadoEquipeEsportiva,
-    String? localSorteio,
-    String? nomeMunicipioUfSorteio,
-    String? nomeTimeCoracaoMesSorte,
-    int? numero,
-    int? numeroConcursoAnterior,
-    int? numeroConcursoFinal05,
-    int? numeroConcursoProximo,
-    int? numeroJogo,
-    String? observacao,
-    dynamic premiacaoContingencia,
-    String? tipoJogo,
-    int? tipoPublicacao,
-    bool? ultimoConcurso,
-    double? valorArrecadado,
-    double? valorAcumuladoConcurso05,
-    double? valorAcumuladoConcursoEspecial,
-    double? valorAcumuladoProximoConcurso,
-    double? valorEstimadoProximoConcurso,
-    double? valorSaldoReservaGarantidora,
-    double? valorTotalPremioFaixaUm,
-  }) =>
-      MegaSenaModel(
-        acumulado: acumulado ?? this.acumulado,
-        dataApuracao: dataApuracao ?? this.dataApuracao,
-        dataProximoConcurso: dataProximoConcurso ?? this.dataProximoConcurso,
-        dezenasSorteadasOrdemSorteio:
-            dezenasSorteadasOrdemSorteio ?? this.dezenasSorteadasOrdemSorteio,
-        exibirDetalhamentoPorCidade:
-            exibirDetalhamentoPorCidade ?? this.exibirDetalhamentoPorCidade,
-        id: id ?? this.id,
-        indicadorConcursoEspecial:
-            indicadorConcursoEspecial ?? this.indicadorConcursoEspecial,
-        listaDezenas: listaDezenas ?? this.listaDezenas,
-        listaDezenasSegundoSorteio:
-            listaDezenasSegundoSorteio ?? this.listaDezenasSegundoSorteio,
-        listaMunicipioUfGanhadores:
-            listaMunicipioUfGanhadores ?? this.listaMunicipioUfGanhadores,
-        listaRateioPremio: listaRateioPremio ?? this.listaRateioPremio,
-        listaResultadoEquipeEsportiva:
-            listaResultadoEquipeEsportiva ?? this.listaResultadoEquipeEsportiva,
-        localSorteio: localSorteio ?? this.localSorteio,
-        nomeMunicipioUfSorteio:
-            nomeMunicipioUfSorteio ?? this.nomeMunicipioUfSorteio,
-        nomeTimeCoracaoMesSorte:
-            nomeTimeCoracaoMesSorte ?? this.nomeTimeCoracaoMesSorte,
-        numero: numero ?? this.numero,
-        numeroConcursoAnterior:
-            numeroConcursoAnterior ?? this.numeroConcursoAnterior,
-        numeroConcursoFinal05:
-            numeroConcursoFinal05 ?? this.numeroConcursoFinal05,
-        numeroConcursoProximo:
-            numeroConcursoProximo ?? this.numeroConcursoProximo,
-        numeroJogo: numeroJogo ?? this.numeroJogo,
-        observacao: observacao ?? this.observacao,
-        premiacaoContingencia:
-            premiacaoContingencia ?? this.premiacaoContingencia,
-        tipoJogo: tipoJogo ?? this.tipoJogo,
-        tipoPublicacao: tipoPublicacao ?? this.tipoPublicacao,
-        ultimoConcurso: ultimoConcurso ?? this.ultimoConcurso,
-        valorArrecadado: valorArrecadado ?? this.valorArrecadado,
-        valorAcumuladoConcurso05:
-            valorAcumuladoConcurso05 ?? this.valorAcumuladoConcurso05,
-        valorAcumuladoConcursoEspecial: valorAcumuladoConcursoEspecial ??
-            this.valorAcumuladoConcursoEspecial,
-        valorAcumuladoProximoConcurso:
-            valorAcumuladoProximoConcurso ?? this.valorAcumuladoProximoConcurso,
-        valorEstimadoProximoConcurso:
-            valorEstimadoProximoConcurso ?? this.valorEstimadoProximoConcurso,
-        valorSaldoReservaGarantidora:
-            valorSaldoReservaGarantidora ?? this.valorSaldoReservaGarantidora,
-        valorTotalPremioFaixaUm:
-            valorTotalPremioFaixaUm ?? this.valorTotalPremioFaixaUm,
-      );
+    ModelMegaSena copyWith({
+        String? loteria,
+        int? concurso,
+        String? data,
+        String? local,
+        List<String>? dezenasOrdemSorteio,
+        List<String>? dezenas,
+        List<dynamic>? trevos,
+        String? timeCoracao,
+        String? mesSorte,
+        List<Premiacoe>? premiacoes,
+        List<dynamic>? estadosPremiados,
+        String? observacao,
+        bool? acumulou,
+        int? proximoConcurso,
+        String? dataProximoConcurso,
+        List<dynamic>? localGanhadores,
+        double? valorArrecadado,
+        double? valorAcumuladoConcurso05,
+        double? valorAcumuladoConcursoEspecial,
+        double? valorAcumuladoProximoConcurso,
+        double? valorEstimadoProximoConcurso,
+    }) => 
+        ModelMegaSena(
+            loteria: loteria ?? this.loteria,
+            concurso: concurso ?? this.concurso,
+            data: data ?? this.data,
+            local: local ?? this.local,
+            dezenasOrdemSorteio: dezenasOrdemSorteio ?? this.dezenasOrdemSorteio,
+            dezenas: dezenas ?? this.dezenas,
+            trevos: trevos ?? this.trevos,
+            timeCoracao: timeCoracao ?? this.timeCoracao,
+            mesSorte: mesSorte ?? this.mesSorte,
+            premiacoes: premiacoes ?? this.premiacoes,
+            estadosPremiados: estadosPremiados ?? this.estadosPremiados,
+            observacao: observacao ?? this.observacao,
+            acumulou: acumulou ?? this.acumulou,
+            proximoConcurso: proximoConcurso ?? this.proximoConcurso,
+            dataProximoConcurso: dataProximoConcurso ?? this.dataProximoConcurso,
+            localGanhadores: localGanhadores ?? this.localGanhadores,
+            valorArrecadado: valorArrecadado ?? this.valorArrecadado,
+            valorAcumuladoConcurso05: valorAcumuladoConcurso05 ?? this.valorAcumuladoConcurso05,
+            valorAcumuladoConcursoEspecial: valorAcumuladoConcursoEspecial ?? this.valorAcumuladoConcursoEspecial,
+            valorAcumuladoProximoConcurso: valorAcumuladoProximoConcurso ?? this.valorAcumuladoProximoConcurso,
+            valorEstimadoProximoConcurso: valorEstimadoProximoConcurso ?? this.valorEstimadoProximoConcurso,
+        );
 
-  factory MegaSenaModel.fromJson(Map<String, dynamic> json) => MegaSenaModel(
-        acumulado: json["acumulado"],
-        dataApuracao: json["dataApuracao"],
-        dataProximoConcurso: json["dataProximoConcurso"],
-        dezenasSorteadasOrdemSorteio: List<String>.from(
-            json["dezenasSorteadasOrdemSorteio"].map((x) => x)),
-        exibirDetalhamentoPorCidade: json["exibirDetalhamentoPorCidade"],
-        id: json["id"],
-        indicadorConcursoEspecial: json["indicadorConcursoEspecial"],
-        listaDezenas: List<String>.from(json["listaDezenas"].map((x) => x)),
-        listaDezenasSegundoSorteio: json["listaDezenasSegundoSorteio"],
-        listaMunicipioUfGanhadores: List<dynamic>.from(
-            json["listaMunicipioUFGanhadores"].map((x) => x)),
-        listaRateioPremio: List<ListaRateioPremio>.from(
-            json["listaRateioPremio"]
-                .map((x) => ListaRateioPremio.fromJson(x))),
-        listaResultadoEquipeEsportiva: json["listaResultadoEquipeEsportiva"],
-        localSorteio: json["localSorteio"],
-        nomeMunicipioUfSorteio: json["nomeMunicipioUFSorteio"],
-        nomeTimeCoracaoMesSorte: json["nomeTimeCoracaoMesSorte"],
-        numero: json["numero"],
-        numeroConcursoAnterior: json["numeroConcursoAnterior"],
-        numeroConcursoFinal05: json["numeroConcursoFinal_0_5"],
-        numeroConcursoProximo: json["numeroConcursoProximo"],
-        numeroJogo: json["numeroJogo"],
+    factory ModelMegaSena.fromJson(Map<String, dynamic> json) => ModelMegaSena(
+        loteria: json["loteria"],
+        concurso: json["concurso"],
+        data: json["data"],
+        local: json["local"],
+        dezenasOrdemSorteio: List<String>.from(json["dezenasOrdemSorteio"].map((x) => x)),
+        dezenas: List<String>.from(json["dezenas"].map((x) => x)),
+        trevos: List<dynamic>.from(json["trevos"].map((x) => x)),
+        timeCoracao: json["timeCoracao"],
+        mesSorte: json["mesSorte"],
+        premiacoes: List<Premiacoe>.from(json["premiacoes"].map((x) => Premiacoe.fromJson(x))),
+        estadosPremiados: List<dynamic>.from(json["estadosPremiados"].map((x) => x)),
         observacao: json["observacao"],
-        premiacaoContingencia: json["premiacaoContingencia"],
-        tipoJogo: json["tipoJogo"],
-        tipoPublicacao: json["tipoPublicacao"],
-        ultimoConcurso: json["ultimoConcurso"],
+        acumulou: json["acumulou"],
+        proximoConcurso: json["proximoConcurso"],
+        dataProximoConcurso: json["dataProximoConcurso"],
+        localGanhadores: List<dynamic>.from(json["localGanhadores"].map((x) => x)),
         valorArrecadado: json["valorArrecadado"],
-        valorAcumuladoConcurso05:
-            json["valorAcumuladoConcurso_0_5"]?.toDouble(),
-        valorAcumuladoConcursoEspecial:
-            json["valorAcumuladoConcursoEspecial"]?.toDouble(),
-        valorAcumuladoProximoConcurso:
-            json["valorAcumuladoProximoConcurso"]?.toDouble(),
+        valorAcumuladoConcurso05: json["valorAcumuladoConcurso_0_5"]?.toDouble(),
+        valorAcumuladoConcursoEspecial: json["valorAcumuladoConcursoEspecial"]?.toDouble(),
+        valorAcumuladoProximoConcurso: json["valorAcumuladoProximoConcurso"]?.toDouble(),
         valorEstimadoProximoConcurso: json["valorEstimadoProximoConcurso"],
-        valorSaldoReservaGarantidora: json["valorSaldoReservaGarantidora"],
-        valorTotalPremioFaixaUm: json["valorTotalPremioFaixaUm"],
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
-        "acumulado": acumulado,
-        "dataApuracao": dataApuracao,
-        "dataProximoConcurso": dataProximoConcurso,
-        "dezenasSorteadasOrdemSorteio":
-            List<dynamic>.from(dezenasSorteadasOrdemSorteio.map((x) => x)),
-        "exibirDetalhamentoPorCidade": exibirDetalhamentoPorCidade,
-        "id": id,
-        "indicadorConcursoEspecial": indicadorConcursoEspecial,
-        "listaDezenas": List<dynamic>.from(listaDezenas.map((x) => x)),
-        "listaDezenasSegundoSorteio": listaDezenasSegundoSorteio,
-        "listaMunicipioUFGanhadores":
-            List<dynamic>.from(listaMunicipioUfGanhadores.map((x) => x)),
-        "listaRateioPremio":
-            List<dynamic>.from(listaRateioPremio.map((x) => x.toJson())),
-        "listaResultadoEquipeEsportiva": listaResultadoEquipeEsportiva,
-        "localSorteio": localSorteio,
-        "nomeMunicipioUFSorteio": nomeMunicipioUfSorteio,
-        "nomeTimeCoracaoMesSorte": nomeTimeCoracaoMesSorte,
-        "numero": numero,
-        "numeroConcursoAnterior": numeroConcursoAnterior,
-        "numeroConcursoFinal_0_5": numeroConcursoFinal05,
-        "numeroConcursoProximo": numeroConcursoProximo,
-        "numeroJogo": numeroJogo,
+    Map<String, dynamic> toJson() => {
+        "loteria": loteria,
+        "concurso": concurso,
+        "data": data,
+        "local": local,
+        "dezenasOrdemSorteio": List<dynamic>.from(dezenasOrdemSorteio.map((x) => x)),
+        "dezenas": List<dynamic>.from(dezenas.map((x) => x)),
+        "trevos": List<dynamic>.from(trevos.map((x) => x)),
+        "timeCoracao": timeCoracao,
+        "mesSorte": mesSorte,
+        "premiacoes": List<dynamic>.from(premiacoes.map((x) => x.toJson())),
+        "estadosPremiados": List<dynamic>.from(estadosPremiados.map((x) => x)),
         "observacao": observacao,
-        "premiacaoContingencia": premiacaoContingencia,
-        "tipoJogo": tipoJogo,
-        "tipoPublicacao": tipoPublicacao,
-        "ultimoConcurso": ultimoConcurso,
+        "acumulou": acumulou,
+        "proximoConcurso": proximoConcurso,
+        "dataProximoConcurso": dataProximoConcurso,
+        "localGanhadores": List<dynamic>.from(localGanhadores.map((x) => x)),
         "valorArrecadado": valorArrecadado,
         "valorAcumuladoConcurso_0_5": valorAcumuladoConcurso05,
         "valorAcumuladoConcursoEspecial": valorAcumuladoConcursoEspecial,
         "valorAcumuladoProximoConcurso": valorAcumuladoProximoConcurso,
         "valorEstimadoProximoConcurso": valorEstimadoProximoConcurso,
-        "valorSaldoReservaGarantidora": valorSaldoReservaGarantidora,
-        "valorTotalPremioFaixaUm": valorTotalPremioFaixaUm,
-      };
+    };
 }
 
-class ListaRateioPremio {
-  final String descricaoFaixa;
-  final int faixa;
-  final int numeroDeGanhadores;
-  final double valorPremio;
+class Premiacoe {
+    final String descricao;
+    final int faixa;
+    final int ganhadores;
+    final double valorPremio;
 
-  ListaRateioPremio({
-    required this.descricaoFaixa,
-    required this.faixa,
-    required this.numeroDeGanhadores,
-    required this.valorPremio,
-  });
+    Premiacoe({
+        required this.descricao,
+        required this.faixa,
+        required this.ganhadores,
+        required this.valorPremio,
+    });
 
-  ListaRateioPremio copyWith({
-    String? descricaoFaixa,
-    int? faixa,
-    int? numeroDeGanhadores,
-    double? valorPremio,
-  }) =>
-      ListaRateioPremio(
-        descricaoFaixa: descricaoFaixa ?? this.descricaoFaixa,
-        faixa: faixa ?? this.faixa,
-        numeroDeGanhadores: numeroDeGanhadores ?? this.numeroDeGanhadores,
-        valorPremio: valorPremio ?? this.valorPremio,
-      );
+    Premiacoe copyWith({
+        String? descricao,
+        int? faixa,
+        int? ganhadores,
+        double? valorPremio,
+    }) => 
+        Premiacoe(
+            descricao: descricao ?? this.descricao,
+            faixa: faixa ?? this.faixa,
+            ganhadores: ganhadores ?? this.ganhadores,
+            valorPremio: valorPremio ?? this.valorPremio,
+        );
 
-  factory ListaRateioPremio.fromJson(Map<String, dynamic> json) =>
-      ListaRateioPremio(
-        descricaoFaixa: json["descricaoFaixa"],
+    factory Premiacoe.fromJson(Map<String, dynamic> json) => Premiacoe(
+        descricao: json["descricao"],
         faixa: json["faixa"],
-        numeroDeGanhadores: json["numeroDeGanhadores"],
+        ganhadores: json["ganhadores"],
         valorPremio: json["valorPremio"]?.toDouble(),
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
-        "descricaoFaixa": descricaoFaixa,
+    Map<String, dynamic> toJson() => {
+        "descricao": descricao,
         "faixa": faixa,
-        "numeroDeGanhadores": numeroDeGanhadores,
+        "ganhadores": ganhadores,
         "valorPremio": valorPremio,
-      };
+    };
 }
